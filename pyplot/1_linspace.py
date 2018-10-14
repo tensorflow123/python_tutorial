97425fb83bd29f81a7a53b7727c7cca57c63a486
xie.kunming <kunming.xie@hotmail.com>
2018-10-14 16:28:56 +0800

    add linspace example

---
+++ pyplot/1_linspace.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# numpy.linspace(start, stop, num = 50, endpoint = True, retstep = False, dtype = None) : Returns number spaces evenly w.r.t interval. Similiar to arange but instead of step it uses sample number.
+
+x = np.linspace(0, 1, 5)
+
+print(x)
