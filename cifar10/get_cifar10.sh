ead75ef016377c8ca75d88a38b499d57955950b5
kunming.xie <kunming.xie@hotmail.com>
2018-11-21 21:01:11 +0800

    do not unzip

--- cifar10/get_cifar10.sh
+++ cifar10/get_cifar10.sh
@@ -8,10 +8,10 @@ echo "Downloading..."
 
 wget --no-check-certificate http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz
 
-echo "Unzipping..."
+# echo "Unzipping..."
 
-tar -xf cifar-10-binary.tar.gz && rm -f cifar-10-binary.tar.gz
-mv cifar-10-batches-bin/* . && rm -rf cifar-10-batches-bin
+# tar -xf cifar-10-binary.tar.gz && rm -f cifar-10-binary.tar.gz
+# mv cifar-10-batches-bin/* . && rm -rf cifar-10-batches-bin
 
 # Creation is split out because leveldb sometimes causes segfault
 # and needs to be re-created.
